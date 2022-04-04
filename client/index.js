d4bfccff6252476153116276b84dea8a8635b8b9
nehreen <nehreenanamatgmaildotcom>
2022-04-04 14:29:03 -0400

    incorporating redux store into provider

--- client/index.js
+++ client/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import { store } from './app/store';
+import { Provider } from 'react-redux';
 
 render(
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>,
-  document.getElementById('app'),
+  document.getElementById('app')
 );
