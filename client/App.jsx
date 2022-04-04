330ed2787760707dad63b6bd96ecc89b372ddcf0
Ari Shoham <arishoham@gmail.com>
2022-04-04 12:26:35 -0400

    file structure boilerplate setup
    Co-Authored-By: Robert Beier <robert.f.beier@gmail.com>

---
+++ client/App.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+
+import './stylesheets/styles.css';
+
+const App = props => {
+  return (
+    <div className="router">
+      <main>
+        <h1>Hello world</h1>
+      </main>
+    </div>
+  );
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+/* eslint-disable no-unused-vars */
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: [
+    // entry point of our app
+    './client/index.js',
+  ],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  devtool: 'eval-source-map',
+  mode: process.env.NODE_ENV,
+  devServer: {
+    // host: '0.0.0.0',
+    // host: 'localhost',
+    port: 8080,
+    hot: true,
+    historyApiFallback: true,
+
+    static: {
+      // Required for Docker to work with dev server
+      // match the output path
+      directory: path.resolve(__dirname, 'dist'),
+      // match the output 'publicPath'
+      publicPath: '/',
+    },
+
+    headers: { 'Access-Control-Allow-Origin': '*' },
+    proxy: {
+      '/api/**': {
+        target: 'http://localhost:3000/',
+        secure: false,
+      },
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './client/index.html',
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  resolve: {
+    // Enable importing JS / JSX files without specifying their extension
+    extensions: ['.js', '.jsx'],
+  },
+};
